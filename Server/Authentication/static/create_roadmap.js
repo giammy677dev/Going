a17c5060b735263bbb5e1602e8317295ca45181a
giammy677dev <giammy677@gmail.com>
2022-08-13 17:44:54 +0200

    Lettura e personalizzazione Marker Google Maps pronti

--- Server/Authentication/static/create_roadmap.js
+++ Server/Authentication/static/create_roadmap.js
@@ -1,23 +1,24 @@
+var map;
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = {lat:40.733334, lng:14.533333};
+    const italy = { lat: 40.733334, lng: 14.533333 };
     // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
-        center: uluru,
+        center: italy,
         disableDefaultUI: true,
     });
+
     // The marker, positioned at Uluru
-    var stages=[]
+    var stages = []
     map.addListener('click', function (e) {
         console.log(e);
-        if(e.placeId)
-        { //nodo già esistente su google maps
+        if (e.placeId) { //nodo già esistente su google maps
 
             //stages.add({})
             alert("already existing")
-        }else{
+        } else {
             alert("ex novo")
             //nodo ex novo
         }
@@ -27,6 +28,33 @@ function initMap() {
             draggable: false
         });
     });
+//}
+
+// Create a <script> tag and set the USGS URL as the source.
+const script = document.createElement("script");
+
+// This example uses a local copy of the GeoJSON stored at
+// http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+script.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+document.getElementsByTagName("head")[0].appendChild(script);
 }
-  
-  window.initMap = initMap;
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+const eqfeed_callback = function (results) {
+    for (let i = 0; i < results.features.length; i++) {
+        const coords = results.features[i].geometry.coordinates;
+        const latLng = new google.maps.LatLng(coords[1], coords[0]);
+
+        var iconBase = './static/markerProva.png'
+        new google.maps.Marker({
+            position: latLng,
+            map: map,
+            icon: iconBase
+        });
+    }
+};
+
+window.initMap = initMap;
+window.eqfeed_callback = eqfeed_callback;
