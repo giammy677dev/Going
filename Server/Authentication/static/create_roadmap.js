47362c00e8754e5222b9c4742bf09943eadebc07
giammy677dev <giammy677@gmail.com>
2022-08-16 10:52:25 +0200

    Introdotto visualizzazione zoom e de-zoom nei marker custom

--- Server/Authentication/static/create_roadmap.js
+++ Server/Authentication/static/create_roadmap.js
@@ -1,4 +1,7 @@
 var map;
+let customMarker = './storage/markerDiego.png'
+var markers = [];
+
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
@@ -17,44 +20,60 @@ function initMap() {
         if (e.placeId) { //nodo già esistente su google maps
 
             //stages.add({})
-            alert("already existing")
+            //alert("already existing")
         } else {
-            alert("ex novo")
+            //alert("ex novo")
             //nodo ex novo
         }
+
         let marker = new google.maps.Marker({
             map: map,
             position: e.latLng,
             draggable: false
         });
     });
-//}
 
-// Create a <script> tag and set the USGS URL as the source.
-const script = document.createElement("script");
+    /* Change markers on zoom */
+    map.addListener('zoom_changed', function () {
+        var zoom = map.getZoom();
+        console.log(zoom)
+        // iterate over markers and call setVisible
+        for (var i = 0; i < markers.length; i++) {
+            if (zoom <= 5) {
+                markers[i].setVisible(false);
+            }
+            else {
+                markers[i].setVisible(true);
+            }
+        }
+    });
 
-// This example uses a local copy of the GeoJSON stored at
-// http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-script.src =
-    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
-document.getElementsByTagName("head")[0].appendChild(script);
+    // Create a <script> tag and set the USGS URL as the source.
+    const script = document.createElement("script");
+
+    // This example uses a local copy of the GeoJSON stored at
+    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+    script.src =
+        "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
+// Loop through the results array and place a marker for each set of coordinates.
 const eqfeed_callback = function (results) {
     for (let i = 0; i < results.features.length; i++) {
         const coords = results.features[i].geometry.coordinates;
         const latLng = new google.maps.LatLng(coords[1], coords[0]);
 
-        var iconBase = './static/markerProva.png'
-        new google.maps.Marker({
+        let marker = new google.maps.Marker({
             position: latLng,
             map: map,
-            icon: iconBase
+            icon: customMarker,
+            visible: false
         });
+
+        markers.push(marker);
     }
-};
+}
 
 window.initMap = initMap;
 window.eqfeed_callback = eqfeed_callback;
